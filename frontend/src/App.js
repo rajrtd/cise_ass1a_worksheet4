862fc430bc472b08de8839d88e687f2007d75b14
Raj Rathod <rajrasikrathod@gmail.com>
2022-08-25 17:40:03 +1200

    all routes working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,15 +19,15 @@ const App = () =>  {
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
             <ul className="header">
                 <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+                <li><NavLink to = "SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/Submit-Article">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
             <Routes>
-            <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
-            <Route  path="/SubmitArticle" component={SubmitArticle}/>
-            <Route exact path="/404" component={NotFoundPage}/>
+            <Route exact path="/" element={<Home/>}/>
+              <Route  path="/SEPractice" element={<SEPractice/>}/>
+              <Route  path="/Submit-Article" element={<SubmitArticle/>}/>
+              <Route path="*" element={<NotFoundPage/>} replace />
             </Routes>
           </div>
         </div>
